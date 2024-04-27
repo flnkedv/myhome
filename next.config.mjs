6bdb4d1862917f2a61e7591926349b2f3e01d6e2
flnkedv <reborn_fluke@hotmail.co.th>
2024-04-27 10:00:43 +0700

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //distDir: "build", //this line will tell the build to create a file with this name
+
+    //distDir: "build", 
+    
+    //this line will tell the build to create a file with this name
+    //ใส่ตอนที่ต้องการสร้างfolder buildแยก เพื่อจะเอาลงserver 
+    //แต่ถ้าลงgitก็ไม่ต้อง buildธรรมดา ตัว.nextมันเอง
 };
 
 export default nextConfig;
