d972af3e2d7188241c6eab2d52f6740044c915f6
flnkedv <reborn_fluke@hotmail.co.th>
2024-05-07 13:14:55 +0700

    modified:   .env 	modified:   app/layout.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { CustomProviders } from "./provider";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <CustomProviders>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <SpeedInsights />
+        </body>
       </html>
     </CustomProviders>
   );
