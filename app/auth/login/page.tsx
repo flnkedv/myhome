e0723cac86654cada229e963a6535d2c3772e6cf
flnkedv <reborn_fluke@hotmail.co.th>
2024-05-07 14:00:30 +0700

    modified:   app/auth/login/page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,30 +2,33 @@
 import React from "react";
 import { useRef } from "react";
 import { signIn } from "next-auth/react";
+import Button from "@mui/material/Button";
 
 export default function Page() {
-    const username = useRef<HTMLInputElement>(null)
-    const password = useRef<HTMLInputElement>(null)
+  const username = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
 
-    const handleLogin = () => {
-        signIn("credentials",{
-            username: username.current?.value,
-            password: password.current?.value,
-            redirect: true,
-            callbackUrl: '/'
-        })
-    }
+  const handleLogin = () => {
+    signIn("credentials", {
+      username: username.current?.value,
+      password: password.current?.value,
+      redirect: true,
+      callbackUrl: "/",
+    });
+  };
   return (
     <div>
       <h1>Login Page</h1>
       <label>
-        Username
-        <input name="username" type="text" ref={username} />
+        Username <br />
+        <input name="username" type="text" ref={username} /> <br />
       </label>
       <label>
-        Password
-        <input name="password" type="password" ref={password} />
-        <button type="button" onClick={handleLogin}>Login</button>
+        Password <br />
+        <input name="password" type="password" ref={password} /> <br />
+        <Button variant="contained" type="button" onClick={handleLogin}>
+          Login
+        </Button>
       </label>
     </div>
   );
